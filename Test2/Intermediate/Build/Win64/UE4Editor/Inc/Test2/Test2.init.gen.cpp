8398dc54e99725b778ecf37d688541920b835272
Nolan Roher <maniacalspy@gmail.com>
2020-02-07 15:08:11 -0600

    Fixes engine build data

--- Test2/Intermediate/Build/Win64/UE4Editor/Inc/Test2/Test2.init.gen.cpp
+++ Test2/Intermediate/Build/Win64/UE4Editor/Inc/Test2/Test2.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeTest2_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x1DF0750E,
-				0xA0CE00BC,
+				0xA63C3AB6,
+				0x49483DC0,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
