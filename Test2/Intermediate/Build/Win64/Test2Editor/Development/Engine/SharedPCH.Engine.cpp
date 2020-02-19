546ac452972845bf7a17bfb819fe386b8600804a
Adrian Burk <adrian.burk@loop.colum.edu>
2020-02-19 17:43:06 -0600

    Sound Effects change
    Shortened power up sound and added it to second puzzle. Added footsteps to blueprint as well as temp footsteps

--- Test2/Intermediate/Build/Win64/Test2Editor/Development/Engine/SharedPCH.Engine.cpp
+++ Test2/Intermediate/Build/Win64/Test2Editor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "C:/Users/Nolan/Documents/GitHub/Nudge/Test2/Intermediate/Build/Win64/Test2Editor/Development/Engine/SharedPCH.Engine.h"
+#include "C:/Users/Adrian Burk/Documents/GitHub/Nudge/Test2/Intermediate/Build/Win64/Test2Editor/Development/Engine/SharedPCH.Engine.h"
