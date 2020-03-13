4a25fad9a9e20f8d33e39d0580e3b2068b3f573f
Adrian Burk <adrian.burk@loop.colum.edu>
2020-03-13 11:06:31 -0500

    Added Suit Hang-up Sound
    Changed IA_Pod header and cpp file to include an audio component and placed sound effect

--- Test2/Intermediate/Build/Win64/UE4Editor/Inc/Test2/IA_SuitPod.generated.h
+++ Test2/Intermediate/Build/Win64/UE4Editor/Inc/Test2/IA_SuitPod.generated.h
@@ -13,10 +13,10 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 #endif
 #define TEST2_IA_SuitPod_generated_h
 
-#define Test2_Source_Test2_IA_SuitPod_h_15_SPARSE_DATA
-#define Test2_Source_Test2_IA_SuitPod_h_15_RPC_WRAPPERS
-#define Test2_Source_Test2_IA_SuitPod_h_15_RPC_WRAPPERS_NO_PURE_DECLS
-#define Test2_Source_Test2_IA_SuitPod_h_15_INCLASS_NO_PURE_DECLS \
+#define Test2_Source_Test2_IA_SuitPod_h_16_SPARSE_DATA
+#define Test2_Source_Test2_IA_SuitPod_h_16_RPC_WRAPPERS
+#define Test2_Source_Test2_IA_SuitPod_h_16_RPC_WRAPPERS_NO_PURE_DECLS
+#define Test2_Source_Test2_IA_SuitPod_h_16_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesAIA_SuitPod(); \
 	friend struct Z_Construct_UClass_AIA_SuitPod_Statics; \
@@ -25,7 +25,7 @@ public: \
 	DECLARE_SERIALIZER(AIA_SuitPod)
 
 
-#define Test2_Source_Test2_IA_SuitPod_h_15_INCLASS \
+#define Test2_Source_Test2_IA_SuitPod_h_16_INCLASS \
 private: \
 	static void StaticRegisterNativesAIA_SuitPod(); \
 	friend struct Z_Construct_UClass_AIA_SuitPod_Statics; \
@@ -34,7 +34,7 @@ public: \
 	DECLARE_SERIALIZER(AIA_SuitPod)
 
 
-#define Test2_Source_Test2_IA_SuitPod_h_15_STANDARD_CONSTRUCTORS \
+#define Test2_Source_Test2_IA_SuitPod_h_16_STANDARD_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API AIA_SuitPod(const FObjectInitializer& ObjectInitializer); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AIA_SuitPod) \
@@ -47,7 +47,7 @@ private: \
 public:
 
 
-#define Test2_Source_Test2_IA_SuitPod_h_15_ENHANCED_CONSTRUCTORS \
+#define Test2_Source_Test2_IA_SuitPod_h_16_ENHANCED_CONSTRUCTORS \
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
 	NO_API AIA_SuitPod(AIA_SuitPod&&); \
@@ -58,33 +58,34 @@ DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AIA_SuitPod); \
 	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AIA_SuitPod)
 
 
-#define Test2_Source_Test2_IA_SuitPod_h_15_PRIVATE_PROPERTY_OFFSET \
+#define Test2_Source_Test2_IA_SuitPod_h_16_PRIVATE_PROPERTY_OFFSET \
 	FORCEINLINE static uint32 __PPO__MeshComponent() { return STRUCT_OFFSET(AIA_SuitPod, MeshComponent); } \
 	FORCEINLINE static uint32 __PPO__FullMesh() { return STRUCT_OFFSET(AIA_SuitPod, FullMesh); } \
-	FORCEINLINE static uint32 __PPO__MeshMaterials() { return STRUCT_OFFSET(AIA_SuitPod, MeshMaterials); }
+	FORCEINLINE static uint32 __PPO__MeshMaterials() { return STRUCT_OFFSET(AIA_SuitPod, MeshMaterials); } \
+	FORCEINLINE static uint32 __PPO__Suitoff() { return STRUCT_OFFSET(AIA_SuitPod, Suitoff); }
 
 
-#define Test2_Source_Test2_IA_SuitPod_h_12_PROLOG
-#define Test2_Source_Test2_IA_SuitPod_h_15_GENERATED_BODY_LEGACY \
+#define Test2_Source_Test2_IA_SuitPod_h_13_PROLOG
+#define Test2_Source_Test2_IA_SuitPod_h_16_GENERATED_BODY_LEGACY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	Test2_Source_Test2_IA_SuitPod_h_15_PRIVATE_PROPERTY_OFFSET \
-	Test2_Source_Test2_IA_SuitPod_h_15_SPARSE_DATA \
-	Test2_Source_Test2_IA_SuitPod_h_15_RPC_WRAPPERS \
-	Test2_Source_Test2_IA_SuitPod_h_15_INCLASS \
-	Test2_Source_Test2_IA_SuitPod_h_15_STANDARD_CONSTRUCTORS \
+	Test2_Source_Test2_IA_SuitPod_h_16_PRIVATE_PROPERTY_OFFSET \
+	Test2_Source_Test2_IA_SuitPod_h_16_SPARSE_DATA \
+	Test2_Source_Test2_IA_SuitPod_h_16_RPC_WRAPPERS \
+	Test2_Source_Test2_IA_SuitPod_h_16_INCLASS \
+	Test2_Source_Test2_IA_SuitPod_h_16_STANDARD_CONSTRUCTORS \
 public: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
-#define Test2_Source_Test2_IA_SuitPod_h_15_GENERATED_BODY \
+#define Test2_Source_Test2_IA_SuitPod_h_16_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	Test2_Source_Test2_IA_SuitPod_h_15_PRIVATE_PROPERTY_OFFSET \
-	Test2_Source_Test2_IA_SuitPod_h_15_SPARSE_DATA \
-	Test2_Source_Test2_IA_SuitPod_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
-	Test2_Source_Test2_IA_SuitPod_h_15_INCLASS_NO_PURE_DECLS \
-	Test2_Source_Test2_IA_SuitPod_h_15_ENHANCED_CONSTRUCTORS \
+	Test2_Source_Test2_IA_SuitPod_h_16_PRIVATE_PROPERTY_OFFSET \
+	Test2_Source_Test2_IA_SuitPod_h_16_SPARSE_DATA \
+	Test2_Source_Test2_IA_SuitPod_h_16_RPC_WRAPPERS_NO_PURE_DECLS \
+	Test2_Source_Test2_IA_SuitPod_h_16_INCLASS_NO_PURE_DECLS \
+	Test2_Source_Test2_IA_SuitPod_h_16_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
