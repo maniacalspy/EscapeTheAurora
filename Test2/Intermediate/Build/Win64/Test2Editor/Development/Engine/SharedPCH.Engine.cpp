eb16dea91e57713657ba8e937ae6f890fc293db9
Aiden1113 <56145780+Aiden1113@users.noreply.github.com>
2020-05-06 18:10:02 -0500

    NewVFX
    This one works lol

--- Test2/Intermediate/Build/Win64/Test2Editor/Development/Engine/SharedPCH.Engine.cpp
+++ Test2/Intermediate/Build/Win64/Test2Editor/Development/Engine/SharedPCH.Engine.cpp
@@ -1,1 +1,1 @@
-#include "E:/Nudge/Test2/Intermediate/Build/Win64/Test2Editor/Development/Engine/SharedPCH.Engine.h"
+#include "H:/Unreal/Aurora/Nudge/Test2/Intermediate/Build/Win64/Test2Editor/Development/Engine/SharedPCH.Engine.h"
